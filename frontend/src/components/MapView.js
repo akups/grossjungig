b9c63ecb5b248624a6a439cbc768ae0390ec7950
akups <appiah-lindberg@outlook.com>
2020-08-05 07:37:30 +0200

    rooms.lat added to marker

--- frontend/src/components/MapView.js
+++ frontend/src/components/MapView.js
@@ -48,7 +48,7 @@ function MyComponent() {
     <LoadScript googleMapsApiKey={process.env.REACT_APP_MY_MAP_API_KEY}>
       <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={10}>
         {/* Child components, such as markers, info windows, etc. */}
-        {rooms && <Marker onLoad={onLoad} position={{ lat, lng }} />}
+        {rooms.lat && <Marker onLoad={onLoad} position={{ lat, lng }} />}
         <></>
       </GoogleMap>
     </LoadScript>
