1b0eb9c7bab68f8d853b1641fd8963a17e4eecc2
akups <appiah-lindberg@outlook.com>
2020-09-02 06:15:38 +0200

    cleaning up frogotpassword router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -15,7 +15,7 @@ const roomsRoutes = require("./rooms");
 
 router.use("/api/auth", usersRoutes);
 router.use("/api", roomsRoutes);
-router.use("/api/auth/forgotPassword", forgotPasswordRoutes);
+router.use("/forgotPassword", forgotPasswordRoutes);
 // router.use("/api", commentsRoutes);
 
 module.exports = router;
