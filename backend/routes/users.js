73abd310b0a91f2ee060e95395f466aa1233f7cd
akups <appiah-lindberg@outlook.com>
2020-10-08 15:11:19 +0200

    change user route for sign up login

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -56,9 +56,9 @@ router.post("/signup", (req, res) => {
 router.post("/login", (req, res, next) => {
   console.log("backenduser", req.body);
   passport.authenticate("local", (err, user, info) => {
-    // if (err) {
-    //   return res.status(500).json({ message: "Error while logging in" });
-    // }
+    if (err) {
+      return res.status(500).json({ message: "Error while logging in" });
+    }
     if (!user) {
       // no user found with username or password didn't match
       return res.status(400).json({ message: info.message });
@@ -67,9 +67,9 @@ router.post("/login", (req, res, next) => {
     req.login(user, (err) => {
       console.log("USER???????", user);
 
-      if (err) {
-        return res.status(500).json({ message: "Error while logging in" });
-      }
+      // if (err) {
+      //   return res.status(500).json({ message: "Error while logging in" });
+      // }
       // req.user = user; //setting user to be the requested user
       res.json(user);
     });
