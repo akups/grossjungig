aabdd3c143ae6ae69c39b21ec3ec27ce78eac7c5
akups <appiah-lindberg@outlook.com>
2020-10-19 14:50:49 +0200

    user rout modification

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -10,46 +10,45 @@ router.post("/signup", (req, res) => {
   const { name, email, password, role } = req.body;
   console.log(req.body);
 
-  User.findOne({ email: email })
-    .then((found) => {
-      //console.log("FOUND?", found);
-      if (found) {
-        return res.status(400).json({ message: "This email already exists" });
-      }
-      return bcrypt
-        .genSalt()
-        .then((salt) => {
-          return bcrypt.hash(password, salt);
-        })
-        .then((hash) => {
-          console.log(name, role, email, password);
-          console.log("before creating user");
-          return User.create({
-            name: name,
-            role: role,
-            email: email,
-            password: hash,
-          });
-        })
-        .then((newUser) => {
-          console.log("HELLO", newUser);
-          // passport login
-          console.log("before automatic login");
-          req.login(newUser, (err) => {
-            if (err) {
-              console.log("error login", err);
-              res.status(500).json({ message: "Error while logging in" });
-            } else {
-              console.log("sending back the user");
-              res.json(newUser);
-            }
-          });
+  User.findOne({ email: email }).then((found) => {
+    //console.log("FOUND?", found);
+    if (found) {
+      return res.status(400).json({ message: "This email already exists" });
+    }
+    return bcrypt
+      .genSalt()
+      .then((salt) => {
+        return bcrypt.hash(password, salt);
+      })
+      .then((hash) => {
+        console.log(name, role, email, password);
+        console.log("before creating user");
+        return User.create({
+          name: name,
+          role: role,
+          email: email,
+          password: hash,
         });
-    })
-    .catch((err) => {
-      console.log("nothing workerd", err);
-      res.status(500).json({ message: "Error while authorizing" });
-    });
+      })
+      .then((newUser) => {
+        console.log("HELLO", newUser);
+        // passport login
+        console.log("before automatic login");
+        req.login(newUser, (err) => {
+          if (err) {
+            console.log("error login", err);
+            res.status(500).json({ message: "Error while logging in" });
+          } else {
+            console.log("sending back the user");
+            res.json(newUser);
+          }
+        });
+      });
+  });
+  // .catch((err) => {
+  //   console.log("nothing workerd", err);
+  //   res.status(500).json({ message: "Error while authorizing" });
+  // });
 });
 
 //3)
@@ -58,8 +57,7 @@ router.post("/login", (req, res, next) => {
   passport.authenticate("local", (err, user, info) => {
     if (err) {
       return res.status(500).json({ message: "Error while logging in" });
-    }
-    if (!user) {
+    } else if (!user) {
       // no user found with username or password didn't match
       return res.status(400).json({ message: info.message });
     }
