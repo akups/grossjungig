d80b5069d5e92c9f72ea0522c8c8dd4ed316f26d
akups <appiah-lindberg@outlook.com>
2020-10-27 06:27:58 +0100

    merging conflicts and cleaning up

--- backend/routes/forgotPassword.js
+++ backend/routes/forgotPassword.js
@@ -35,51 +35,12 @@ router.post("/forgotPassword", async (req, res) => {
 
   const user = await User.findOne({
     email: req.body.email,
-  }).then((user) => {
-    if (user === null) {
-      console.error("email not in database");
-      res
-        .status(200)
-        .send("If the email is in our database you will receive an email.");
-    } else {
-      const token = crypto.randomBytes(20).toString("hex");
-      user.update({
-        resetPasswordToken: token,
-        resetPasswordExpires: Date.now() + 3600000,
-      });
-
-      const host =
-        process.env.NODE_ENV === "development"
-          ? "http://localhost:3000"
-          : "https://grossjungig.vercel.app/";
-
-      const mailOptions = {
-        from: "grossjungigplatform@gmail.com",
-        to: `${user.email}`,
-        subject: "Link To Reset Password",
-        text:
-          "You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n" +
-          "Please click on the following link, or paste this into your browser to complete the process within one hour of receiving it:\n\n" +
-          `${host}/reset/${token}\n\n` +
-          "If you did not request this, please ignore this email and your password will remain unchanged.\n",
-      };
-
-      console.log("sending mail");
-
-      transporter.sendMail(mailOptions, (err, response) => {
-        if (err) {
-          console.error("there was an error: ", err);
-        } else {
-          console.log("here is the res: ", response);
-          res.status(200).json("recovery email sent");
-        }
-      });
-    }
   });
-
   if (user === null) {
     console.error("email not in database");
-    res.status(200).send("email not in db");
+    res
+      .status(200)
+      .send("If the email is in our database you will receive an email.");
   } else {
     const token = crypto.randomBytes(20).toString("hex");
 
